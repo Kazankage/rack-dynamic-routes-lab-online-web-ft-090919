b55cf8048ee2d39e6710cc8d1f82dfd2d113772d
Randel Jiao <randeljiao@gmail.com>
2019-10-16 18:08:58 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/item"
+      resp.write "ITEM NAME_PRICE"
+    else
+      resp.write "Route not found"
+      resp.status = 400
+    end
+ 
+    resp.finish
+  end
+end
